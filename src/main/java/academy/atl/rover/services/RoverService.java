a9a4c2d8f74f793ab0822faab450624b487f6b53
Matias Nicolas Acevedo <matias.nicolas.acevedo@gmail.com>
2024-01-03 22:26:44 -0300

    Se agrega crud a Interface Rover

--- src/main/java/academy/atl/rover/services/RoverService.java
+++ src/main/java/academy/atl/rover/services/RoverService.java
@@ -1,12 +1,21 @@
 package academy.atl.rover.services;
 
 import academy.atl.rover.models.Rover;
-
+import java.util.List;
 
 public interface RoverService {
 
-    public Rover get();
+    Rover get();
 
     void sendCommand(String command);
 
+    List<Rover> getAll(); // Método para obtener todos los Rovers
+
+    Rover getById(Long id); // Método para obtener un Rover por su ID
+
+    Rover create(Rover rover); // Método para crear un nuevo Rover
+
+    Rover update(Long id, Rover updatedRover); // Método para actualizar un Rover existente
+
+    void delete(Long id); // Método para eliminar un Rover por su ID
 }
