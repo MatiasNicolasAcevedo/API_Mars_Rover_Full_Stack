3cc0d71eb32115a2a20ae745898df8932c71008c
Matias Nicolas Acevedo <matias.nicolas.acevedo@gmail.com>
2024-01-04 13:18:40 -0300

    Se agrega metodos a la interfaz obstacle

--- src/main/java/academy/atl/rover/services/ObstacleService.java
+++ src/main/java/academy/atl/rover/services/ObstacleService.java
@@ -3,8 +3,18 @@ package academy.atl.rover.services;
 import academy.atl.rover.models.Obstacle;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ObstacleService {
-    public List<Obstacle> findAll();
 
+    List<Obstacle> findAll(); // Método para obtener todos los obstáculos
+
+    Optional<Obstacle> findById(Long id); // Método para obtener un obstáculo por su ID
+
+    Obstacle create(Obstacle obstacle); // Método para crear un nuevo obstáculo
+
+    Obstacle update(Long id, Obstacle updatedObstacle); // Método para actualizar un obstáculo existente
+
+    void delete(Long id); // Método para eliminar un obstáculo por su ID
 }
+
